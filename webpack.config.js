43d24b6c0ae233ef1657395e5100ed43777f7638
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-03-01 16:40:45 +0200

    calendar

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,11 @@ module.exports = (env, argv) => {
       new HtmlWebpackPlugin({
         template: './src/index.html',
       }),
+      new CopyPlugin({
+        patterns: [
+          { from: '_redirects', to: '' },
+        ],
+      }),
     ],
     devServer: {
       historyApiFallback: true,
